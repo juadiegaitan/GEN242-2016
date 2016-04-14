9d05cfaf32753ae7ec23443c14dba17d762ad397
tgirke <tgirke@citrus.ucr.edu>
2016-04-14 08:38:35 -0700

    some edits

--- vignettes/06_Homework/mydoc_homework_04.R
+++ vignettes/06_Homework/mydoc_homework_04.R
@@ -7,12 +7,12 @@ y <- substring(y, 1:nchar(y), 1:nchar(y))
 ## Gap penality
 gp <- 8 # Gap penalty
 
-## Create dynamic programming matrix base on input sequences
+## Create dynamic programming matrix based on input sequences
 ma <- matrix(NA, length(y)+1, length(x)+1, dimnames=list(c("gp", y), c("gp", x)))
 ma[1,] <- seq(0, -(length(ma[1,])-1) * gp, -gp)
 ma[,1] <- seq(0, -(length(ma[,1])-1) * gp, -gp)
 ma
 
 ## If desired, write ma to tabular file 
-write.table(ma, quote=FALSE, na = "", sep="\t")
+write.table(ma, file="ma.xls", quote=FALSE, na = "", col.names = NA, sep="\t")
 
