e8316bc6828a01247e6a797aa0307d79ce49abf3
tgirke <tgirke@citrus.ucr.edu>
2016-04-14 08:03:34 -0700

    some edits

---
+++ vignettes/06_Homework/mydoc_homework_04.R
@@ -0,0 +1,18 @@
+## Define and vectorize Input sequences
+x <- "FIPFSAGPRNCIGQK"
+x <- substring(x, 1:nchar(x), 1:nchar(x))
+y <- "PFGFGKRSCMGRRLA"
+y <- substring(y, 1:nchar(y), 1:nchar(y))
+
+## Gap penality
+gp <- 8 # Gap penalty
+
+## Create dynamic programming matrix base on input sequences
+ma <- matrix(NA, length(y)+1, length(x)+1, dimnames=list(c("gp", y), c("gp", x)))
+ma[1,] <- seq(0, -(length(ma[1,])-1) * gp, -gp)
+ma[,1] <- seq(0, -(length(ma[,1])-1) * gp, -gp)
+ma
+
+## If desired, write ma to tabular file 
+write.table(ma, quote=FALSE, na = "", sep="\t")
+
