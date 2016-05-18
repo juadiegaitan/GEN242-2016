351658ab8d2d419e9310062237e3964be27ff7ba
Thomas Girke <tgirke@citrus.ucr.edu>
2016-05-18 16:35:29 -0700

    some edits

---
+++ vignettes/14_Clustering/Rclustering.R
@@ -0,0 +1,12 @@
+## ----style, echo = FALSE, results = 'asis'-------------------------------
+BiocStyle::markdown()
+options(width=100, max.print=1000)
+knitr::opts_chunk$set(
+    eval=as.logical(Sys.getenv("KNITR_EVAL", "TRUE")),
+    cache=as.logical(Sys.getenv("KNITR_CACHE", "TRUE")))
+
+## ----hclust_heatmap_example, eval=TRUE, warning=FALSE, message=FALSE-----
+library(gplots) 
+y <- matrix(rnorm(500), 100, 5, dimnames=list(paste("g", 1:100, sep=""), paste("t", 1:5, sep=""))) 
+heatmap.2(y) # Shortcut to final result
+
